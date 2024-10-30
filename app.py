65dca9fadf2d325d4fd06901ae601304da81b64d
Denis Volk <denis.volk@gmail.com>
2024-10-30 22:30:06 +0000

    feat/refactor/fix

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
+import collections
+import datetime
+import os
+
 from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from ebooklib import epub
 import ebooklib
 from bleach import clean, sanitizer
-import os
+from tqdm import tqdm
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///reader.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Database Models
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String, unique=True, nullable=False)
+    author = db.Column(db.String, unique=False, nullable=False)
     title = db.Column(db.String, nullable=False)
+    chapters_count = db.Column(db.Integer, nullable=False)
     chapters = db.relationship("Chapter", backref="book", lazy=True)
     progress = db.relationship("BookProgress", backref="book", uselist=False)
 
@@ -30,6 +38,7 @@ class Chapter(db.Model):
 class BookProgress(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     chapter_index = db.Column(db.Integer, nullable=False)
+    updated_datetime = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     book_id = db.Column(
         db.Integer, db.ForeignKey("book.id"), nullable=False, unique=True
     )
@@ -40,21 +49,22 @@ BOOKS_DIR = "./epub"
 
 def load_books():
     existing_filenames = {book.filename for book in Book.query.all()}
-    for filename in os.listdir(BOOKS_DIR):
+    for filename in tqdm(os.listdir(BOOKS_DIR)):
         if filename.endswith(".epub") and filename not in existing_filenames:
             print(f"processing {filename}")
             book_path = os.path.join(BOOKS_DIR, filename)
-            book_epub = epub.read_epub(book_path)
+            book_epub = epub.read_epub(book_path, {"ignore_ncx": True})
+            # print(book_epub.metadata)
             title = (
                 book_epub.get_metadata("DC", "title")[0][0]
                 if book_epub.get_metadata("DC", "title")
                 else "Untitled"
             )
-
-            # Create a new Book entry
-            new_book = Book(filename=filename, title=title)
-            db.session.add(new_book)
-            db.session.commit()
+            author = (
+                book_epub.get_metadata("DC", "creator")[0][0]
+                if book_epub.get_metadata("DC", "creator")
+                else "Unknown Author"
+            )
 
             # Extract chapters
             chapters = [
@@ -62,9 +72,13 @@ def load_books():
                 for item in book_epub.get_items()
                 if item.get_type() == ebooklib.ITEM_DOCUMENT
             ]
+            processed_chapters = []
             allowed_tags = list(sanitizer.ALLOWED_TAGS) + ["p", "img"]
             for index, chapter in enumerate(chapters):
-                content = chapter.get_content().decode()
+                try:
+                    content = chapter.get_content().decode()
+                except Exception:
+                    content = chapter.get_content()
                 clean_content = clean(
                     content,
                     tags=allowed_tags,
@@ -74,17 +88,41 @@ def load_books():
                     index=index,
                     title=f"Chapter {index + 1}",
                     content=clean_content,
-                    book_id=new_book.id,
                 )
-                db.session.add(new_chapter)
+                processed_chapters.append(new_chapter)
+            # Create a new Book entry
+            new_book = Book(
+                filename=filename,
+                title=title,
+                author=author,
+                chapters=processed_chapters,
+                chapters_count=len(processed_chapters),
+            )
+            db.session.add(new_book)
             db.session.commit()
 
 
 @app.route("/")
 def index():
     load_books()  # Add new books to the database if they are not already present
-    books = Book.query.all()
-    return render_template("index.jinja2", books=books)
+    books = Book.query.order_by(Book.author, Book.title).all()
+    books2 = collections.defaultdict(list)
+    for book in books:
+        if not book.progress:
+            books2["unread"].append(book)
+        if book.progress:
+            if book.progress.chapter_index + 1 >= book.chapters_count:
+                books2["finished"].append(book)
+            else:
+                books2["in_progress"].append(book)
+    books = {
+        "in_progress": sorted(
+            [b for b in books if b.progress],
+            key=lambda bb: bb.progress.updated_datetime,
+        ),
+        "unread": [b for b in books if not b.progress],
+    }
+    return render_template("index.jinja2", books=books2)
 
 
 @app.route("/book/<int:book_id>/chapter/<int:chapter_index>")
@@ -101,6 +139,7 @@ def read_chapter(book_id, chapter_index):
         db.session.add(progress)
     else:
         progress.chapter_index = chapter_index
+        progress.updated_datetime = datetime.datetime.utcnow()
     db.session.commit()
 
     total_chapters = Chapter.query.filter_by(book_id=book_id).count()
@@ -109,6 +148,7 @@ def read_chapter(book_id, chapter_index):
         title=book.title,
         content=chapter.content,
         book_id=book_id,
+        book=book,
         chapter_index=chapter_index,
         total_chapters=total_chapters,
     )
@@ -126,6 +166,5 @@ def continue_reading(book_id):
 if __name__ == "__main__":
     # Create the database tables and load books from disk
     with app.app_context():
-        db.create_all()
         load_books()
     app.run(port=5438, debug=True)
