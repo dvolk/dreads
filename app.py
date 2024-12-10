434d5c4e646b549ba86a0016004cd6ace6867f09
Denis Volk <denis.volk@gmail.com>
2024-12-10 11:36:15 +0000

    feat: Add tags and search

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import secrets
 import threading
 import time
 import pathlib
+import collections
+import shlex
 
 import argh
 import ebooklib
@@ -26,17 +28,32 @@ from flask_login import (
 )
 from werkzeug.security import generate_password_hash, check_password_hash
 import waitress
+import tabulate
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///reader.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SECRET_KEY"] = secrets.token_urlsafe(64)
+app.config["SECRET_KEY"] = os.environ.get(
+    "CATREADS_SECRET_KEY", secrets.token_urlsafe(64)
+)
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 99999999999
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
 load_books_event = threading.Event()
+
+book_tags = db.Table(
+    "book_tags",
+    db.Column("book_id", db.Integer, db.ForeignKey("book.id"), primary_key=True),
+    db.Column("tag_id", db.Integer, db.ForeignKey("tag.id"), primary_key=True),
+)
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+
 
 # Database Models
 class Book(db.Model):
@@ -45,8 +62,15 @@ class Book(db.Model):
     author = db.Column(db.String, unique=False, index=True, nullable=False)
     title = db.Column(db.String, nullable=False)
     chapters_count = db.Column(db.Integer, nullable=False)
+    is_hidden = db.Column(db.Boolean, nullable=False, default=False)
     chapters = db.relationship("Chapter", backref="book", lazy=True)
     progresses = db.relationship("BookProgress", backref="book")
+    tags = db.relationship(
+        "Tag",
+        secondary=book_tags,
+        backref=db.backref("books", lazy="dynamic"),
+        lazy="dynamic",
+    )
 
 
 class Chapter(db.Model):
@@ -129,6 +153,11 @@ BOOKS_DIR = "./epub"
 
 
 def save_cover_image(book_id):
+    static_dir = pathlib.Path("./static")
+    static_dir.mkdir(exist_ok=True)
+    cover_path = static_dir / f"cover-{book_id}.jpg"
+    if cover_path.is_file():
+        return
     # Try to find the cover ID from metadata
     book = Book.query.get_or_404(book_id)
     book_path = os.path.join(BOOKS_DIR, book.filename)
@@ -153,68 +182,70 @@ def save_cover_image(book_id):
                     cover_item = item
                     break
     if cover_item:
-        static_dir = pathlib.Path("./static")
-        static_dir.mkdir(exist_ok=True)
-        cover_path = static_dir / f"cover-{book_id}.jpg"
         with open(cover_path, "wb") as f:
             f.write(cover_item.get_content())
     else:
+        cover_path.symlink_to("no-cover.png")
         print(f"No cover image found for {book_path}.")
 
 
 def load_books():
     existing_filenames = {book.filename for book in Book.query.all()}
     for filename in tqdm(os.listdir(BOOKS_DIR)):
-        if filename.endswith(".epub") and filename not in existing_filenames:
-            print(f"processing {filename}")
-            book_path = os.path.join(BOOKS_DIR, filename)
-            book_epub = epub.read_epub(book_path, {"ignore_ncx": True})
-            # print(book_epub.metadata)
-            title = (
-                book_epub.get_metadata("DC", "title")[0][0].strip()
-                if book_epub.get_metadata("DC", "title")
-                else "Untitled"
-            )
-            author = (
-                book_epub.get_metadata("DC", "creator")[0][0].strip()
-                if book_epub.get_metadata("DC", "creator")
-                else "Unknown Author"
-            )
+        try:
+            if filename.endswith(".epub") and filename not in existing_filenames:
+                print(f"processing {filename}")
+                book_path = os.path.join(BOOKS_DIR, filename)
+                book_epub = epub.read_epub(book_path, {"ignore_ncx": True})
+                # print(book_epub.metadata)
+                title = (
+                    book_epub.get_metadata("DC", "title")[0][0].strip()
+                    if book_epub.get_metadata("DC", "title")
+                    else "Untitled"
+                )
+                author = (
+                    book_epub.get_metadata("DC", "creator")[0][0].strip()
+                    if book_epub.get_metadata("DC", "creator")
+                    else "Unknown Author"
+                )
 
-            # Extract chapters
-            chapters = [
-                item
-                for item in book_epub.get_items()
-                if item.get_type() == ebooklib.ITEM_DOCUMENT
-            ]
-            processed_chapters = []
-            allowed_tags = list(sanitizer.ALLOWED_TAGS) + ["p", "img"]
-            for index, chapter in enumerate(chapters):
-                try:
-                    content = chapter.get_content().decode()
-                except Exception:
-                    content = chapter.get_content()
-                clean_content = clean(
-                    content,
-                    tags=allowed_tags,
-                    strip=True,
+                # Extract chapters
+                chapters = [
+                    item
+                    for item in book_epub.get_items()
+                    if item.get_type() == ebooklib.ITEM_DOCUMENT
+                ]
+                processed_chapters = []
+                allowed_tags = list(sanitizer.ALLOWED_TAGS) + ["p", "img"]
+                for index, chapter in enumerate(chapters):
+                    try:
+                        content = chapter.get_content().decode()
+                    except Exception:
+                        content = chapter.get_content()
+                    clean_content = clean(
+                        content,
+                        tags=allowed_tags,
+                        strip=True,
+                    )
+                    new_chapter = Chapter(
+                        index=index,
+                        title=f"Chapter {index + 1}",
+                        content=clean_content,
+                    )
+                    processed_chapters.append(new_chapter)
+                # Create a new Book entry
+                new_book = Book(
+                    filename=filename,
+                    title=title,
+                    author=author,
+                    chapters=processed_chapters,
+                    chapters_count=len(processed_chapters),
                 )
-                new_chapter = Chapter(
-                    index=index,
-                    title=f"Chapter {index + 1}",
-                    content=clean_content,
-                )
-                processed_chapters.append(new_chapter)
-            # Create a new Book entry
-            new_book = Book(
-                filename=filename,
-                title=title,
-                author=author,
-                chapters=processed_chapters,
-                chapters_count=len(processed_chapters),
-            )
-            db.session.add(new_book)
-            db.session.commit()
+                db.session.add(new_book)
+                db.session.commit()
+        except Exception as e:
+            print(filename)
+            print(str(e))
 
 
 @app.route("/apply_settings")
@@ -285,10 +316,97 @@ def inject_globals():
     }
 
 
+@app.route("/hide/<int:book_id>")
+def hide(book_id):
+    book = Book.query.get_or_404(book_id)
+    book.is_hidden = True
+    for p in BookProgress.query.filter_by(book_id=book_id):
+        db.session.delete(p)
+    db.session.commit()
+    return redirect("index")
+
+
+def add_tags(book_id, tag_names):
+    with app.app_context():
+        book = Book.query.filter_by(id=book_id).first()
+        tag_names = [tn.strip() for tn in tag_names.split(",")]
+        # go through user-supplied tags and add tags to books, creating
+        # new tags if they don't already exist
+        book.tags = []
+        for tag_name in tag_names:
+            existing_tag = Tag.query.filter_by(name=tag_name).first()
+            if existing_tag and existing_tag not in list(book.tags):
+                book.tags.append(existing_tag)
+            elif not existing_tag:
+                book.tags.append(Tag(name=tag_name))
+        db.session.commit()
+
+
+@app.route("/add_tags", methods=["POST"])
+@login_required
+def add_tags_route():
+    book_id = request.form.get("book_id")
+    Book.query.get_or_404(book_id)
+    tag_names = request.form.get("tag_names", "")
+    add_tags(book_id, tag_names)
+    return redirect(url_for("continue_reading", book_id=book_id))
+
+
+def parse_query(text, default_key="title"):
+    # parse a search query, like title:"my title" tag:mytag into
+    # { 'title': ['my title'], 'tag': ['mytag'] }
+    out = collections.defaultdict(list)
+    try:
+        tokens = shlex.split(text)
+    except ValueError as e:
+        logging.exception("Error parsing query: ")
+        return out
+
+    for token in tokens:
+        if ":" in token:
+            key, value = token.split(":", 1)
+            if v := value.strip():
+                out[key.lower()].append(v)
+        else:
+            if v := token.strip():
+                out[default_key].append(v)
+    return out
+
+
 @app.route("/")
 @login_required
 def index():
-    books = Book.query.order_by(Book.author, Book.title).all()
+    books = Book.query.order_by(Book.author, Book.title)
+    q = request.args.get("q", "")
+    filters = parse_query(q)
+
+    if request.args.get("show_all") != "y":
+        books = books.filter_by(is_hidden=False)
+
+    # process search query for tags, author and title
+    for filter_tag_name in filters.get("tag", []):
+        if filter_tag_name.startswith("!"):
+            negated_tag = filter_tag_name[1:]
+            books = books.filter(~Book.tags.any(Tag.name == negated_tag))
+        else:
+            books = books.filter(Book.tags.any(Tag.name == filter_tag_name))
+
+    for filter_author_name in filters.get("author", []):
+        if filter_author_name.startswith("!"):
+            negated_author = filter_author_name[1:]
+            books = books.filter(~Book.author.ilike(f"%{negated_author}%"))
+        else:
+            books = books.filter(Book.author.ilike(f"%{filter_author_name}%"))
+
+    for filter_title_name in filters.get("title", []):
+        if filter_title_name.startswith("!"):
+            negated_title = filter_title_name[1:]
+            books = books.filter(~Book.title.ilike(f"%{negated_title}%"))
+        else:
+            books = books.filter(Book.title.ilike(f"%{filter_title_name}%"))
+
+    books = books.all()
+
     user_progresses = BookProgress.query.filter_by(user_id=current_user.id)
     user_progresses = {up.book_id: up for up in user_progresses}
     unread_books = []
@@ -296,6 +414,7 @@ def index():
     in_progress_books = []
     now = datetime.datetime.utcnow()
 
+    # sort book list into in progress, unread and finished books
     for book in books:
         if not book.id in user_progresses:
             unread_books.append(book)
@@ -308,6 +427,7 @@ def index():
                     (now - book_progress.updated_datetime, book, book_progress)
                 )
 
+    # sort by last read
     in_progress_books = sorted(in_progress_books, key=lambda bb: bb[0])
 
     return render_template(
@@ -316,6 +436,7 @@ def index():
         finished_books=finished_books,
         in_progress_books=in_progress_books,
         humanize=humanize,
+        q=q,
     )
 
 
@@ -388,7 +509,7 @@ def trigger_load_books():
     return redirect(url_for("index"))
 
 
-def run_app():
+def run_app(debug=False):
     def load_books_thread():
         while True:
             with app.app_context():
@@ -396,8 +517,11 @@ def run_app():
             load_books_event.wait(timeout=60 * 60)
             load_books_event.clear()
 
-    threading.Thread(target=load_books_thread, daemon=True).start()
-    waitress.serve(app, port=5438)
+    if debug:
+        app.run(port=5438, debug=True)
+    else:
+        threading.Thread(target=load_books_thread, daemon=True).start()
+        waitress.serve(app, port=5438)
 
 
 def process_cover(book_id):
@@ -411,11 +535,35 @@ def process_covers():
             process_cover(book.id)
 
 
+def attr_join(xs, attr=None, sep=","):
+    return sep.join([getattr(x, attr) for x in xs])
+
+
+def show_books():
+    ret = []
+    with app.app_context():
+        for book in Book.query.order_by(Book.title).all():
+            book_tags_str = attr_join(book.tags, "name")
+            if not book_tags_str:
+                ret.append(
+                    [
+                        book.id,
+                        book.is_hidden,
+                        book.author[:20],
+                        book.title[:40],
+                        book_tags_str,
+                    ]
+                )
+    print(tabulate.tabulate(ret))
+
+
 if __name__ == "__main__":
     argh.dispatch_commands(
         [
             run_app,
             process_cover,
             process_covers,
+            show_books,
+            add_tags,
         ]
     )
